c97f5cf7d6452c010f8bc5cc3ce6eda86baf8245
袁杰 <3186698962@qq.com>
2018-07-30 07:31:04 +0800

    'test'

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,18 +8,22 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class PostController extends Controller {
+    // 文章列表
     public function index(){
         return view("post/index");
     }
     public function show(){
-        return view("post/show");
+        return view("post/show",['title'=>'我的标题']);
     }
+    // 创建文章
     public function create(){
         return view("post/create");
     }
     public function store(){
     }
+    // 编辑文章
     public function edit(){
+        return view(view("post/edit"));
     }
     public function update(){
     }
