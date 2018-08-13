73c9010cef76ab5b1ce514a915778e6f2e6e2a6c
yuanjie <yuanjie2018@oomall.com>
2018-08-13 17:13:48 +0800

    '利用模板继承调整页面结构'

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,24 +10,35 @@ use Illuminate\Http\Request;
 class PostController extends Controller {
     // 文章列表
     public function index(){
-        return view("post/index");
+        $posts = [
+            ["title" => "我的标题一"],
+            ["title" => "我的标题二"],
+            ["title" => "我的标题三"],
+            ["title" => "我的标题四"]
+        ];
+        $arr = ["a" => "11111111111","b" => "2222222222","c" => "33333333","d" => "44444444"];
+        extract($arr);
+        return view("post/index",compact('posts','a','b','c','d'));
     }
     public function show(){
-        return view("post/show",['title'=>'我的标题']);
+        return view("post/show",['title'=>'我的标题一','isShow'=>false]);
     }
     // 创建文章
     public function create(){
         return view("post/create");
     }
     public function store(){
+        return;
     }
     // 编辑文章
     public function edit(){
         return view(view("post/edit"));
     }
     public function update(){
+        return;
     }
     public function delete(){
+        return;
     }
 
 }
